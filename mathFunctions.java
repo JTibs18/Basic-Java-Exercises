0c32b0006edfe81037764e571661e76c5f2b3d01
Jtibs18 <jessicalovessoccer18@hotmail.com>
2020-04-26 16:30:01 -0400

    subtraction fuction

--- mathFunctions.java
+++ mathFunctions.java
@@ -2,15 +2,27 @@ import java.util.Scanner;
 
 public class mathFunctions {
   public static void main(String[] args){
+    System.out.println("Welcome to math functions!");
     int num1;
     int num2;
     num1 = reader("first");
     num2 = reader("second");
+
     int result = add(num1, num2);
-    System.out.println("The sum of " + num1 + " + "+ num2 + " = " + result);
+    System.out.println("The sum is " + num1 + " + "+ num2 + " = " + result);
+
+    int difference = subtract(num1, num2);
+    System.out.println("The difference is " + num1 + " - " + num2 + " = " + difference); 
     }
 
-//get the user's input for numbers
+
+
+    //gets the user to select a math function
+//    public static int chooseFunction(){
+//    System.out.println("Please choose a math function:");
+//    }
+
+  //get the user's input for numbers
   public static int reader(String num){
     Scanner iReader = new Scanner (System.in);
     System.out.print("Please enter the " + num + " number: ");
@@ -19,9 +31,16 @@ public class mathFunctions {
     return i;
   }
 
-//function to add numbers 
+  //function to add numbers
   public static int add(int n1, int n2){
     int result = n1 + n2;
     return result;
   }
+
+  //function to subtract numbers
+  public static int subtract(int n1, int n2){
+    int result = n1 - n2;
+    return result;
+  }
+
 }
