a2aa53eea594f166787ff4ff2a88577ddcb8e320
Jtibs18 <jessicalovessoccer18@hotmail.com>
2020-04-26 16:38:17 -0400

    multiply and divide functions

--- mathFunctions.java
+++ mathFunctions.java
@@ -12,11 +12,16 @@ public class mathFunctions {
     System.out.println("The sum is " + num1 + " + "+ num2 + " = " + result);
 
     int difference = subtract(num1, num2);
-    System.out.println("The difference is " + num1 + " - " + num2 + " = " + difference); 
+    System.out.println("The difference is " + num1 + " - " + num2 + " = " + difference);
+
+    int quotient = divide (num1, num2);
+    System.out.println("The quotient is " + num1 + " / " + num2 + " = " + quotient);
+
+    int product = multiply(num1, num2);
+    System.out.println("The product is " + num1 + " * " + num2 + " = " + product);
+
     }
-
-
-
+    
     //gets the user to select a math function
 //    public static int chooseFunction(){
 //    System.out.println("Please choose a math function:");
@@ -31,15 +36,27 @@ public class mathFunctions {
     return i;
   }
 
-  //function to add numbers
+  //function to add two numbers
   public static int add(int n1, int n2){
     int result = n1 + n2;
     return result;
   }
 
-  //function to subtract numbers
+  //function to subtract two numbers
   public static int subtract(int n1, int n2){
     int result = n1 - n2;
+    return result;
+  }
+
+  //function to divide two numbers
+  public static int divide (int n1, int n2){
+    int result = n1/n2;
+    return result;
+  }
+
+  //function to multply two numbers
+  public static int multiply (int n1, int n2){
+    int result = n1*n2;
     return result;
   }
 
